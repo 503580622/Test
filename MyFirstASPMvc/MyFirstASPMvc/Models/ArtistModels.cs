37b74ff3d06988709aab24a894caa6c8d4f837d3
Lee Linton <503580622@qq.com>
2016-05-20 00:24:58 +0800

    建立模型

---
+++ MyFirstASPMvc/MyFirstASPMvc/Models/ArtistModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstASPMvc.Models
+{
+	class Artist
+	{
+		public virtual int ArtistId { get; set; }
+		public virtual string name { get; set; }
+	}
+}
